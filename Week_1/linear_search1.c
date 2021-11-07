ed8fcf49f4e3b3e0e50b1a5edb60653429ed1c65
Thopuri Harshitha <93579922+20WH1A6637@users.noreply.github.com>
2021-11-07 20:43:56 +0530

    Create linear_search1.c
    linear search with recursion

---
+++ Week_1/linear_search1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int LinearSearch1(int[],int,int, int);
+void readArray(int[], int);
+int main(){
+    int a[100],n,ele;
+    int result;
+    printf("enter the no of elements of the array:");
+    scanf("%d",&n);
+    readArray(a, n);
+    printf("enter the value or key to search:");
+    scanf("%d",&ele);
+    result = LinearSearch1(a,n,ele,0);
+    if(result==-1){
+        printf("Element is not found");
+    }
+    else{
+        printf("Element is found at %d position",result);
+    }
+}
+void readArray(int a[], int n){
+    printf("Enter the elements of the array:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+}
+int LinearSearch1(int a[],int n,int key, int i){
+    if(i > n){
+        return -1;
+    }
+    else if(a[i] == key){
+        return i;
+    }
+    return LinearSearch1(a, n, key, i++);
+}
